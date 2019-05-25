c63d5ad83bf44bb71cc147a6ed2b41cd836d7053
varungowda <varun@svikrti.com>
2019-05-25 13:02:11 +0530

    pwa

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function (event) {});
 
 
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
 
 // importScripts('cache-pollyfill.js');
 
